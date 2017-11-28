6e9ed3caa4e41011e16bbf58da1bb5c0f2e85041
StephanGELo <lefortsge@gmail.com>
2017-11-27 21:10:08 -0800

    Implemented stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,28 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+ 
+  if (Array.isArray(obj)) {
+    var answer = [];
+    for (var i = 0; i < obj.length; i++) {
+      answer.push(stringifyJSON(obj[i]));
+    }  
+    return '[' + answer.join(',') + ']';
+  }
+ 
+  if (obj && typeof(obj) === 'object') {
+    var answer2 = [];
+    for (var k in obj) {
+      if (obj[k] === undefined || typeof obj[k] === 'function') {
+        continue;
+      } 
+      answer2.push(stringifyJSON(k) + ':' + stringifyJSON(obj[k]));
+    }
+    return '{' + answer2.join(',') + '}';
+  }
+ 
+  return '' + obj; 
 };
